29e450ce9a9d7b704ef8ed387904cdd95f3c09c1
MostafaFouad <tee.fouad@gmail.com>
2016-06-02 23:16:02 +0200

    optimize and fix a bug

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 var fs = require('fs');
 var resolve = require('path').resolve;
-var sassPath;
 
-function importer(path, prev, done) {
-  var stat, isPartial = true;
-  
-  try {
-    var pathArray = path.split('/');
-    var file = '_' + pathArray.pop();
-    stat = fs.lstatSync(sassPath + '/sass/' + pathArray.join('/') + '/' + file + '.scss');
-  } catch (e) {
-    isPartial = false;
-  }
-  
-  if (!isPartial) {
+var importer = function(sassPath) {
+  return function (path, prev, done) {
+    var stat, isPartial = true;
+    
     try {
-      stat = fs.lstatSync(sassPath + '/sass/' + path + '.scss');
+      var pathArray = path.split('/');
+      var file = '_' + pathArray.pop();
+      stat = fs.lstatSync(sassPath + '/sass/' + pathArray.join('/') + '/' + file + '.scss');
     } catch (e) {
-      return done && done(null);
+      isPartial = false;
     }
-  }
-  
-  if (stat.isFile()) {
-    done && done({
-      file: resolve(sassPath + '/sass/' + path)
-    });
+    
+    if (!isPartial) {
+      try {
+        stat = fs.lstatSync(sassPath + '/sass/' + path + '.scss');
+      } catch (e) {
+        return done();
+      }
+    }
+    
+    if (stat.isFile()) {
+      done({
+        file: resolve(sassPath + '/sass/' + path)
+      });
+    }
   }
 }
 
 module.exports = function(dirname) {
-  sassPath = dirname;
-  return importer;
+  return importer(dirname);
 };
